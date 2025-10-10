60d91bb57b71c8e8e2300209643aa177f9598d6c
Pierre Cochard <pierre.cochard@inria.fr>
2025-10-10 16:32:58 +0200

    noisce

--- src/widgets/waveform.rs
+++ src/widgets/waveform.rs
@@ -4,10 +4,11 @@ use std::{
 };
 
 use crossterm::event::{KeyCode, KeyEvent};
+use rand::Rng;
 use ratatui::{
-    buffer::Buffer, layout::Flex, prelude::Rect, style::Color, symbols, widgets::{
+    buffer::Buffer, layout::Flex, prelude::Rect, style::Color, symbols, text::Text, widgets::{
         canvas::{
-            Canvas, Line, Points
+            Canvas, Label, Line, Points
         }, Widget, WidgetRef
     }
 };
@@ -35,7 +36,6 @@ impl Waveform {
                 .add_button("triangle")
                 .add_button("square")
                 .add_button("noise")
-
         }
     }
 }
@@ -61,10 +61,12 @@ impl InteractiveWidget for Waveform {
         // meaning we have 200 ticks per second:
         let period = Duration::from_millis(5).as_secs_f32();
 
-        let incr = 0.005025;
+        let incr = 0.0050125;
         let amplitude = self.cblock.read_control(0).unwrap() as f64;
         let sine = self.cblock.read_control(1).unwrap();
         let tri = self.cblock.read_control(2).unwrap();
+        let sqr = self.cblock.read_control(3).unwrap();
+        let noise = self.cblock.read_control(4).unwrap();
         let x_offset = 5.5 as f64;
 
         if sine != 0.0 {
@@ -95,8 +97,29 @@ impl InteractiveWidget for Waveform {
                 }
             }
             self.phase = phase;
+        } else if sqr != 0.0 {
+            let mut phase = self.phase;
+            for n in 0..self.coords.len() {
+                let mut y = if phase <= 0.5 {-1.0} else {1.0};
+                y = y * amplitude + 200.0;
+                self.coords[n] = (n as f64 + x_offset, y);
+                phase += incr;
+                if phase >= 1.0 {
+                   phase -= 1.0;
+                }
+            }
+            self.phase = phase;
+        } else if noise != 0.0 {
+            // shift all values left:
+            for n in 0..self.coords.len()-1 {
+                self.coords[n] = (n as f64, self.coords[n+1].1);
+            }
+            let mut rng = rand::rng();
+            let y = rng.random_range(-1.0..1.0);
+            let n = self.coords.len()-1;
+            let last = self.coords.last_mut().unwrap();
+            *last = (n as f64 + x_offset, y * amplitude + 200.0);
         }
-
     }
 }
 
@@ -107,19 +130,38 @@ impl WidgetRef for Waveform {
             .background_color(Color::White)
             .paint(|ctx| {
                 ctx.draw(&Line {
-                    x1: 5.5, y1: 200.0,
-                    x2: 400.0, y2: 200.0,
+                    x1: 10.0, y1: 200.0,
+                    x2: 390.0, y2: 200.0,
+                    color: Color::Gray
+                });
+                ctx.draw(&Line {
+                    x1: 10.0, y1: 300.0,
+                    x2: 390.0, y2: 300.0,
+                    color: Color::Gray
+                });
+                ctx.draw(&Line {
+                    x1: 10.0, y1: 100.0,
+                    x2: 390.0, y2: 100.0,
                     color: Color::Gray
                 });
                 ctx.draw(&Points {
                     coords: &self.coords,
                     color: Color::Black                    
                 });
+                ctx.print(375.0, 305.0, ratatui::text::Line::from("+1V"));
+                ctx.print(375.0, 95.0, ratatui::text::Line::from("-1V"));
                 for n in (0..self.coords.len()).step_by(2) {
+                    // If we have too much of a gap between two values in one sample, 
+                    // draw the line on the same vertical axis:
+                    let x2 = if (self.coords[n].1 - self.coords[n+1].1).abs() >= 0.5 {
+                        self.coords[n].0
+                    } else {
+                        self.coords[n+1].0
+                    };
                     ctx.draw(&Line {
                         x1: self.coords[n].0,
                         y1: self.coords[n].1,
-                        x2: self.coords[n+1].0,
+                        x2: x2,
                         y2: self.coords[n+1].1,
                         color: Color::Black
                     });
